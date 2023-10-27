c24b505ff383291a087ae2738732c3fc2fcc0aa7
Jean-Paulos <110000423+Jean-Paulos@users.noreply.github.com>
2023-10-27 20:36:18 -0300

    Update utils.py
    Adicionado função para exibir tempo médio dos jogadores

--- utils.py
+++ utils.py
@@ -10,6 +10,29 @@ def datilografa(texto, delay=0.1):
         sleep(delay)
     print()
 
+def tempo_para_segundos(tempo):
+    h, m, s = map(int, tempo.split(':'))
+    return h * 3600 + m * 60 + s
+
+def tempo_media():
+    placar = "placar.json"
+
+    try:
+        with open(placar, "r") as arquivo_json:
+            dados_antigos = json.load(arquivo_json)
+    except FileNotFoundError:
+        dados_antigos = []
+
+    df = pd.DataFrame(dados_antigos)
+
+    df['tempo_decorrido_segundos'] = df['Tempo'].apply(tempo_para_segundos)
+
+    tempo_medio = df['tempo_decorrido_segundos'].mean()
+
+    tempo_medio_formatado = str(timedelta(seconds=int(tempo_medio)))
+
+    print(f"Tempo médio dos jogadores: {tempo_medio_formatado}")
+    
 def placar():
     placar = "placar.json"
 
