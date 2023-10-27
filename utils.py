9a3afb322175ec1d7febdc64f2241c417ac5e9bb
Jean-Paulos <110000423+Jean-Paulos@users.noreply.github.com>
2023-10-27 20:35:25 -0300

    Update utils.py
    Adicionado lib do pandas e função para exibir placar de jogador

--- utils.py
+++ utils.py
@@ -1,7 +1,29 @@
-from time import sleep
+import pandas as pd
+import json
+from questionary import select
+from time import sleep, time
+from datetime import timedelta
 
 def datilografa(texto, delay=0.1):
     for letter in texto:
         print(letter, end='', flush=True)
         sleep(delay)
     print()
+
+def placar():
+    placar = "placar.json"
+
+    try:
+        with open(placar, "r") as arquivo_json:
+            dados_antigos = json.load(arquivo_json)
+    except FileNotFoundError:
+        dados_antigos = []
+
+    df = pd.DataFrame(dados_antigos)
+
+    df = df.sort_values(by=["Tempo"])
+
+    df = df.reset_index(drop=True)
+    df.index +=1
+
+    print(df.to_string(index=True))
