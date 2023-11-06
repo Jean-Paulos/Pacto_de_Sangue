6cf3c08e854ab519e108b14a61ef698a7ac3becf
eriksantos777 <144533310+eriksantos777@users.noreply.github.com>
2023-11-06 19:09:29 -0300

    Update text.py
    Adicionado textos do primeiro capítulo

--- text.py
+++ text.py
@@ -1,24 +1,49 @@
-c1_text001 = '''Você abre os olhos lentamente, sentindo uma forte dor de cabeça. 
-Você se vê em uma pequena sala completamente branca.'''
-c1_text002 = '''
-“Onde… onde estou? O que está acontecendo?”
-
-Você se sente angustiado e confuso. 
+#O Inicio
+c1_text001 = '''Você acorda em um lugar estranho, uma pequena sala completamente branca.
+O ambiente parece limpo e estéril, mas sua mente está turbulenta, e você se sente angustiado e confuso. 
 Uma sensação de ansiedade o envolve, deixando-o sem qualquer lembrança de como chegou ali ou por que está ali.'''
-c1_text003 = '''Você se levanta com cautela, suas pernas tremendo pela sensação de fragilidade e vulnerabilidade. 
-Ele sabe que precisa encontrar uma saída, entender como chegou aqui e por que.'''
-c1_text004 = '''Você tenta se acalmar, mas uma onda de medo e ansiedade o invade. 
-Você sente que algo está errado, muito errado.
 
-"Não adianta ficar aqui parado … "
+# Permanecer onde está
+c1_text002 = '''"Não vou conseguir nada desse jeito ..."
+De repente um som estranho ecoa pelas paredes'''
 
-De repente, ele ouve um som estranho ecoando pelas paredes.'''
-c1_text005 = '''Você presta atenção no som, tentando identificar sua origem. 
-Você percebe que são vozes sussurrantes ao fundo. 
-Elas parecem estar conversando sobre algo, mas você não consegue entender o que dizem.
+# Prestar atenção no som
+c1_text003 = '''Você ouve vozes sussurrantes ao fundo. Elas parecem estar conversando sobre algo, mas o som é indistinto.
+Parece haver uma conversa distorcida acontecendo do lado de fora, e isso só aumenta a sensação de urgência em entender sua situação.
 
-“Realmente…” “ele … ?”
+"Você.....que ele sabe?"
 
-“… sei…” “mas…” “... cuidado.”
+"... sei... mas... ...ter cuidado"
 
-As vozes aumentam sua sensação de paranoia e perigo. Você se pergunta quem são essas pessoas e o que elas querem dele.'''
+As vozes continuam a ecoar ao seu redor, tornando-se cada vez mais nítidas e perturbadoras.
+As vozes criam uma sensação de que alguém o está vigiando e planejando algo, deixando-o ainda mais angustiado.'''
+
+# Levantar
+c1_text004 = '''você se levanta com cautela, suas pernas tremendo pela sensação de fragilidade e vulnerabilidade. 
+você sabe que precisa encontrar uma saída, entender como chegou aqui e por que.'''
+
+# Olhar em volta
+c1_text005 = 'Você nota alguns elementos na sala.'
+
+# Sanitario
+c1_text006 = 'Em um canto da sala, há um sanitário simples.'
+# Câmera
+c1_text007 = 'A câmera, escondida nas sombras do canto superior, parece observar cada um de seus movimentos. Você sente-se nu diante de seus olhos, aumentando ainda mais sua sensação de aprisionamento.'
+# Parede
+c1_text008 = 'Examinando a parede, você nota uma porta quase imperceptível. Ela é completamente branca e se mistura à parede. No entanto, você observa uma pequena escotilha na parte inferior.'
+# Cadeira
+c1_text009 = 'A cadeira é uma peça simples de mobília. Ela é feita de um material branco, combinando com o ambiente. Quando você tenta movê-la, você logo percebe que a cadeira está estranhamente presa ao chão, como se tivesse sido fundida com o próprio piso. Seus esforços para deslocá-la são em vão.'
+# Corda suspensa
+c1_text010 = 'Uma corda pendurada no teto chama a sua atenção. Ela tem um nó que dá a impressão de ser usada para se enforcar. A visão dela cria uma sensação ainda mais angustiante em você.'
+# Bater na porta
+c1_text011 = '''Desesperado, você se aproxima da porta, grita e bate com força.
+"Alguém aí? Por favor, me ajudem! Estou preso aqui!"
+Esperando que alguém do lado de fora o escute e responda ao seu chamado. No entanto, a porta permanece sólida e imóvel, como se estivesse selada contra o mundo exterior.
+"Tem alguém lá fora? Qualquer um, por favor, me escute!"'''
+# Continuar batendo e gritando por ajuda
+c1_text012 = '''Você continua gritando, mas não obtém nenhuma resposta além do eco de seus próprios gritos.
+Com o tempo, sua voz começa a ficar rouca e fraca, e você percebe que está ficando exausto com o esforço.'''
+# Tentar se lembrar onde estava
+c1_text013 = '''Você fecha os olhos por um momento na esperança de trazer à tona alguma lembrança do seu passado. As vozes continuam a sussurrar ao seu redor, e a sensação de que algo está acontecendo além das paredes do quarto só aumenta.
+Enquanto se esforça para desenterrar lembranças perdidas, sua mente vagueia até um momento distante e reconfortante: uma tarde de sol em um parque, onde uma risada de criança preenchia o ar. Você se lembra vividamente de estar cercado por sua família, sua filha brincando alegremente enquanto você os observava com um sorriso no rosto.
+Você decide se concentrar na tentativa de lembrar de algo, qualquer detalhe que possa ajudar a desvendar o mistério de como e por que você está preso neste lugar estranho. No entanto, mesmo com esforço, suas memórias parecem inalcançáveis, como se estivessem trancadas em algum lugar da sua mente.'''
