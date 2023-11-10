88f0c861d17b1a8b1c87d22048395b7f0d441768
jonathanppontes <146490671+jonathanppontes@users.noreply.github.com>
2023-11-10 19:47:18 -0300

    Update main.py
    Adicionei os capítulos 3 e 4

--- main.py
+++ main.py
@@ -206,3 +206,188 @@ elif response == 'Ir até a corda':
     choices=["..."])
     response = ask.ask()
     datilografa(c2_text014, delay=0.02)
+# Editado por Jonathan
+datilografa(c3_text001,delay=0.02)
+load_and_listen()
+
+ask = select(
+    "Você tem algumas alternativas:",
+    choices=["Olhar em volta"])
+response = ask.ask()
+if response == 'Olhar em volta':
+    datilografa(c3_text002,delay=0.02)
+    load_and_listen()
+
+ask = select(
+    "Você tem algumas alternativas:",
+    choices=["Ir em direção a mulher"])
+response = ask.ask()
+if response == 'Ir em direção a mulher':
+    datilografa(c3_text003,delay=0.02)
+    load_and_listen()
+
+ask = select(
+    "Você tem algumas alternativas:",
+    choices=["Continuar correndo"])
+response = ask.ask()
+datilografa(c3_text004,delay=0.02)
+load_and_listen()
+
+ask = select(
+    "Você tem algumas alternativas:",
+    choices=["???"])
+response = ask.ask()
+if response == '???':
+    datilografa(c4_text001,delay=0.02)
+    load_and_listen()
+ask = select(
+    "Você tem algumas alternativas:",
+    choices=["Olhar em volta"])
+response = ask.ask()
+if response == 'Olhar em volta':
+    datilografa(c4_text002,delay=0.02)
+    load_and_listen()
+
+choices = ["Porta","Câmera","Sanitario","Cadeira","Diário","Corda suspensa","Parede",]
+# Inicio de Loop
+while True:
+    ask = select(
+        "Você tem algumas alternativas:",
+        choices=choices)
+    response = ask.ask()
+    if response == 'Porta':    
+        datilografa(c4_text027, delay=0.02) #Porta 4-1
+        load_and_listen()      
+        choices.remove("Porta") #remove texto 4-1
+        choices.append("Porta ")
+    elif response == "Porta ":
+        datilografa(c4_text028,delay=0.02) #texto da porta 4-2
+        ask = select(
+            "Você tem algumas alternativas:",
+            choices=["Verificar o papel","Olhar em volta"])
+        response = ask.ask()
+    elif response == 'Diário':
+        datilografa(c4_text007, delay=0.02)
+        load_and_listen()
+    elif response == 'Câmera':
+        datilografa(c4_text004, delay=0.02)
+        load_and_listen()
+    elif response == 'Sanitario':
+        datilografa(c4_text005, delay=0.02) 
+        load_and_listen()
+        ask = select(
+            "Você tem algumas alternativas:",
+            choices=["Cagar"])
+        response = ask.ask()
+        datilografa(c4_text010,delay=0.02)
+        load_and_listen()
+
+    elif response == 'Cadeira':
+        datilografa(c4_text006, delay=0.02)
+        load_and_listen()
+
+        ask = select(
+            "Você tem algumas alternativas:",
+            choices=["Sentar-se na cadeira:","Olhar em volta"])
+        response = ask.ask()
+        if response == "Olhar em volta":
+            continue
+        elif response == "Sentar-se na cadeira:":# texto ok
+            datilografa(c4_text011,delay=0.02) 
+            load_and_listen()
+            
+            ask = select(
+            "Você tem algumas alternativas:",
+            choices=["Escrever no diário","Olhar em volta"])
+            response = ask.ask()
+            if response == "Escrever no diário":
+                datilografa(c4_text014,delay=0.02)
+                c4_text007 = c4_text020 # junta o texto do diario
+
+    if response == "Olhar em volta":
+            continue
+    elif response == "Escrever no diário":# texto ok
+        datilografa(c4_text014,delay=0.02)
+        ask = select(
+            "Você tem algumas alternativas:",
+            choices=["Diário"])
+        response = ask.ask()
+        datilografa(c4_text007,delay=0.02) 
+        load_and_listen()    
+    elif response == 'Corda suspensa':
+        datilografa(c4_text008, delay=0.02)
+        load_and_listen()
+        ask = select(
+            "Você tem algumas alternativas:",
+            choices=["Acabar com tudo","Olhar em volta"])
+        response = ask.ask()
+    if response == "Olhar em volta":#ok
+            continue
+    elif response =="Acabar com tudo":
+        datilografa(fim,delay=0.02)
+        load_and_listen()
+        exit()
+    #Primeira opção terminar loop
+    elif response == 'Parede':
+        datilografa(c4_text009, delay=0.02)# Parede 4-1
+        load_and_listen()
+        ask = select(
+            "Você tem algumas alternativas:",
+            choices=["Desenhar na parede"])
+        response = ask.ask()                   
+        datilografa(c4_text017,delay=0.02)
+        ask = select(
+            "Você tem algumas alternativas:",
+            choices=["Analisar parede"])
+        response = ask.ask()
+        datilografa(c4_text018,delay=0.02)
+        load_and_listen()
+        ask = select(
+            "Você tem algumas alternativas:",
+            choices=["Ir até a porta","Olhar em volta"])
+        response = ask.ask()
+        choices.remove("Parede") #remove texto 4-1
+        choices.append("Parede ")# adicionar porta 4-2
+    elif response == "Parede ":
+        datilografa(c4_text029,delay=0.02) #texto porta 4-2      
+    if response == "Olhar em volta":
+        continue
+    elif response =="Ir até a porta":
+        datilografa(c4_text021,delay=0.02) # Porta 4-2
+        load_and_listen()
+        ask = select(
+            "Você tem algumas alternativas:",
+            choices=["Comer a comida","Olhar em volta"])
+        response = ask.ask()
+    if response == "Olhar em volta":
+        continue        
+    elif response == 'Comer a comida':
+        datilografa(c4_text022, delay=0.02)
+        load_and_listen()
+        ask = select(
+        "Você tem algumas alternativas:",
+        choices=["Guardar o osso do bife", "Guardar a faca", "Terminar de comer"])
+        response = ask.ask()        
+    if response == "Guardar o osso do bife":
+        datilografa(c4_text023, delay=0.02)
+        load_and_listen()
+        ask = select(
+            "Você tem algumas alternativas:",
+            choices=["Terminar de comer"])
+        response = ask.ask()
+    elif response == "Guardar a faca":
+        datilografa(c4_text024, delay=0.02)
+        load_and_listen()
+        ask = select(
+            "Você tem algumas alternativas:",
+            choices=["Terminar de comer"])
+        response = ask.ask()
+    elif response == "Terminar de comer":
+        datilografa(c4_text025, delay=0.02)
+        load_and_listen()
+    elif response =='Verificar o papel':
+        datilografa(c4_text030,delay=0.02)
+        load_and_listen()
+        break
+
+
