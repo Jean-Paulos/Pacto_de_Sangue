47c2b789b0013102fb98d6687cb53e1af07f073e
Danilo <110535677+daniloNatan@users.noreply.github.com>
2023-11-12 11:55:38 -0300

    Adição do Capítulo 5, Sistema de Combate e Menu Interativo
    Neste commit, introduzo a continuação da história no Capítulo 5, expandindo o enredo e aprofundando a experiência narrativa do jogo. O enredo foi refinado para oferecer uma imersão ainda maior aos jogadores.
    
    Além disso, implementei um sistema de combate para adicionar uma camada estratégica à narrativa, permitindo aos jogadores interagirem com o ambiente de forma mais dinâmica. O sistema de combate foi projetado para ser intuitivo, mantendo-se coeso com a trama existente.
    
    Uma melhoria significativa foi a substituição do antigo sistema de opções por um novo menu interativo. Este menu proporciona uma experiência mais fluida e esteticamente agradável, aumentando a usabilidade e a imersão dos jogadores no universo do jogo.
    
    Essas adições são parte de um esforço contínuo para elevar a qualidade geral do projeto, proporcionando aos jogadores uma experiência envolvente e emocionante.

--- main.py
+++ main.py
@@ -390,4 +390,271 @@ while True:
         load_and_listen()
         break
 
+# Inicio capitulo 5
+#[[Destruir bilhete]]
+
+datilografa(c5_text001,delay=0.02)
+datilografa(c5_text002,delay=0.02)
+datilografa(c5_text002_1,delay=0.02)
+datilografa(c5_text002_2,delay=0.02)
+load_and_listen()
+
+ask = select(
+    "Você tem algumas alternativas:",
+    choices= ["Olhar em volta"])
+response = ask.ask()
+
+if response == "Olhar em volta":
+    datilografa(c5_text003,delay=0.02)
+
+choices= ["Porta","Câmera","Sanitario","Cadeira","Parede","Corda suspensa","Diário",""]
+while True: #Regra criada para alterar o texto e "choises" da "Cadeira c5" e "Diário c5"
+
+    ask = select(
+        "Você tem algumas alternativas:",
+        choices=choices)
+    
+    response = ask.ask()
+    #Porta c5
+    if response == 'Porta':
+        datilografa(c5_text004, delay=0.02)
+        load_and_listen()
+    #Câmera c5
+    elif response == 'Câmera':
+        datilografa(c5_text005, delay=0.02)
+        load_and_listen()
+    #Sanitario c5
+    elif response == 'Sanitario':
+        datilografa(c5_text006, delay=0.02)
+        load_and_listen()
+        ask = select(
+            "Você tem algumas alternativas:",
+            choices= ["Cagar","Olhar em volta"]) #Cagar c5 ou Voltar ao Menu (Olhar em volta)
+        response = ask.ask()
+        if response == "Cagar":
+            datilografa(c5_text007, delay=0.02)
+            load_and_listen()
+    #Cadeira c5
+    elif response == 'Cadeira':
+        datilografa(c5_text008, delay=0.02)
+        load_and_listen()
+        ask = select(
+            "Você tem algumas alternativas:",
+            choices=["Sentar-se na cadeira","Olhar em volta"])
+        response = ask.ask()
+        #Sentar-se na cadeira c5
+        if response == 'Sentar-se na cadeira':
+            datilografa(c5_text009, delay=0.02)
+            ask = select(
+                "Você tem algumas alternativas:",
+                choices=["Diário"])
+            response = ask.ask()
+            if response == 'Diário':
+                datilografa(c5_text010,delay=0.01)
+                ask = select(
+                    "Você tem algumas alternativas:",
+                    choices=["Escrever no diário"])
+                response = ask.ask()
+                choices.remove("Diário") #remove texto 4-1
+                choices.append("Diário ")
+                if response == 'Escrever no diário':
+                    c5_text010 = c5_text024
+                    datilografa(c5_text011,delay=0.02)
+                    load_and_listen()
+                    ask = select(
+                    "Você tem algumas alternativas:",
+                    choices=["Continuar escrevendo"])
+                    response = ask.ask()
+                    if response == 'Continuar escrevendo':
+                        datilografa(c5_text012,delay=0.02)
+                        datilografa(c5_text013,delay=0.02)
+                        datilografa(c5_text014,delay=0.02)
+                        datilografa(c5_text015,delay=0.02)
+                        datilografa(c5_text016,delay=0.02)
+                        load_and_listen()
+
+                        ask = select(
+                            "Você tem algumas alternativas:",
+                            choices= ["Refletir"])
+                        response = ask.ask()
+                        if response == 'Refletir':
+                            datilografa(c5_text017,delay=0.02)
+                            datilografa(c5_text018,delay=0.02)
+                            datilografa(c5_text019,delay=0.02)
+                            datilografa(c5_text020,delay=0.02)
+                            datilografa(c5_text021,delay=0.02)
+                            load_and_listen()
+                            choices.remove("")
+                            choices.append("Por plano em prática")
+        
+    #Parede c5
+    elif response == 'Parede':
+        datilografa(c5_text022,delay=0.02)
+        load_and_listen()
+    #Corda suspensa c5
+    elif response == 'Corda suspensa':
+        datilografa(c5_text023,delay=0.02)
+        load_and_listen()
+    #Diário c5
+    elif response == 'Diário':
+        datilografa(c5_text010,delay=0.02)
+        load_and_listen()
+    elif response == 'Diário ':
+        datilografa(c5_text024,delay=0.02)
+        load_and_listen()
+    elif response == '':
+        print('')
+    #Por plano em prática c5
+    elif response == 'Por plano em prática':
+        datilografa(c5_text025,delay=0.02)
+        datilografa(c5_text026,delay=0.02)
+        load_and_listen()
+        ask = select(
+            "O que você fará a seguir?",
+            choices=['Aguardar o momento certo'])
+        response = ask.ask()
+        datilografa(c5_text028,delay=0.02)
+        datilografa(c5_text029,delay=0.02)
+        load_and_listen()
+        ask = select(
+            "O que acontecerá a seguir?",
+            choices=["Atacar agora!","Esperar"])
+        response = ask.ask()
+
+        if response == 'Atacar agora!':
+            datilografa(c5_text030,delay=0.02)
+            datilografa(c5_text031,delay=0.02)
+            datilografa(c5_text068,delay=0.02)
+            load_and_listen()
+            print('''Parabéns, você encontrou uma das nossas rotas exclusivas para a morte! Infelizmente, sua pontuação não será salva desta vez. Se quiser deixar sua marca no nosso ranking, talvez na próxima vez tente não morrer. Boa sorte!''')
+            exit()
+            #Sistema de ataque
+            
+        elif response == 'Esperar':
+            datilografa(c5_text032,delay=0.02)
+            datilografa(c5_text033,delay=0.02)
+            datilografa(c5_text034,delay=0.02)
+            datilografa(c5_text035,delay=0.02)
+            load_and_listen()
+            ask = select(
+                "Pode ser a grande chance!",
+                choices=["Atacar","Esperar por mais..."])
+            response = ask.ask()
+
+            if response == 'Atacar':
+                datilografa(c5_text036,delay=0.02)
+                datilografa(c5_text037,delay=0.02)
+                datilografa(c5_text038,delay=0.02)
+                load_and_listen()
+                #Sistema de ataque
+                ataque = randint(1, 20)#<------ Estima a regra p/ gerar um numero de "sorte" de forma aleatória p/ o persongaem com as chances do ataque de 1 a 20 ser efetivo
+                if ataque in range(1, 5):#<------ Quando o numero gerado estiver entre 1 a 5 ele não será efetivo e vai cair direto no fim do jogo.
+                    datilografa(c5_text030,delay=0.02)                    
+                    datilografa(c5_text031,delay=0.02)                    
+                    datilografa(c5_text068,delay=0.02)
+                    load_and_listen()
+                    datilografa(fim,delay=0.02)
+                    exit() #<------ Fim do jogo
+                elif ataque in range(6, 15):# <------ Gera uma nova chance de ataque.
+                    datilografa(c5_text039,delay=0.02)
+                    load_and_listen()
+                    ataque = randint(1, 20)# <------ Cria a regra para a "sorte" ser gerada entre 5 a 20 de forma aleatória
+                    if ataque in range(1, 3):# <------ Se o numero gerado estiver entre 5 a 15 o jogo se encerra.
+                        datilografa(c5_text031,delay=0.02)
+                        datilografa(c5_text068,delay=0.02)
+                        load_and_listen()
+                        datilografa(fim,delay=0.02)
+                        exit()
+                    elif ataque in range(4, 20):# <------ Se estiver entre 16 a 20 o numero gerado fim do ataque de forma efetiva e a história continua
+                        datilografa(c5_text069,delay=0.02)
+                        datilografa(c5_text070,delay=0.02)
+                        datilografa(c5_text071,delay=0.02)
+                        datilografa(c5_text072,delay=0.02)
+                        datilografa(c5_text073,delay=0.02)
+                        load_and_listen()
+                        datilografa(c5_text074,delay=0.02)
+                        datilografa(c5_text075,delay=0.02)
+                        datilografa(c5_text050,delay=0.02)
+                        datilografa(c5_text051,delay=0.02)
+                        load_and_listen()
+                        datilografa(c5_text052,delay=0.02)
+                        datilografa(c5_text053,delay=0.02)
+                        datilografa(c5_text065,delay=0.02)
+                        datilografa(c5_text066,delay=0.02)
+                        datilografa(c5_text067,delay=0.02)
+                        load_and_listen()
+                        datilografa(fim,delay=0.02)
+                        exit()
+                
+                elif ataque in range(16, 20):# <------ Se estiver entre 16 a 20 o numero gerado fim do ataque de forma efetiva e a história continua
+                    datilografa(c5_text069,delay=0.02)
+                    datilografa(c5_text070,delay=0.02)
+                    datilografa(c5_text071,delay=0.02)
+                    datilografa(c5_text072,delay=0.02)
+                    datilografa(c5_text073,delay=0.02)
+                    load_and_listen()
+                    datilografa(c5_text074,delay=0.02)
+                    datilografa(c5_text075,delay=0.02)
+                    datilografa(c5_text050,delay=0.02)
+                    datilografa(c5_text051,delay=0.02)
+                    datilografa(c5_text052,delay=0.02)
+                    load_and_listen()
+                    datilografa(c5_text053,delay=0.02)
+                    datilografa(c5_text065,delay=0.02)
+                    datilografa(c5_text066,delay=0.02)
+                    datilografa(c5_text067,delay=0.02)
+                    load_and_listen()
+                    datilografa(fim,delay=0.02)
+                    exit()
+
+            elif response == 'Esperar por mais...':
+                datilografa(c5_text040,delay=0.02)
+                datilografa(c5_text041,delay=0.02)
+                load_and_listen()
+                ask = select(
+                    "Faça valer a pena.",
+                    choices=["Atacar!"])
+                response = ask.ask()
+                datilografa(c5_text042,delay=0.02)
+                datilografa(c5_text043,delay=0.02)
+                datilografa(c5_text044,delay=0.02)
+                datilografa(c5_text045,delay=0.02)
+                load_and_listen()
+                datilografa(c5_text046,delay=0.02)
+                datilografa(c5_text047,delay=0.02)
+                datilografa(c5_text048,delay=0.02)
+                datilografa(c5_text049,delay=0.02)
+                load_and_listen()
+
+        datilografa(c5_text054,delay=0.02)
+        load_and_listen()
+        datilografa(c5_text055,delay=0.02)
+        load_and_listen()
+        datilografa(c5_text056,delay=0.02)
+        load_and_listen()
+        datilografa(c5_text057,delay=0.02)
+        load_and_listen()
+        datilografa(c5_text058,delay=0.02)
+        load_and_listen()
+        datilografa(c5_text059,delay=0.02)
+        load_and_listen()
+        datilografa(c5_text060,delay=0.02)
+        load_and_listen()
+        datilografa(c5_text061,delay=0.02)
+        load_and_listen()
+        datilografa(c5_text062,delay=0.02)
+        load_and_listen()
+        datilografa(c5_text063,delay=0.02)
+        load_and_listen()
+        datilografa(c5_text064,delay=0.02)
+        load_and_listen()
+
+        if response == 'A corda':
+            datilografa(c5_text065,delay=0.02)
+            datilografa(c5_text066,delay=0.02)
+            datilografa(c5_text067,delay=0.02)
+            load_and_listen()
+            fim2 = fim2.format(tempo_formatado)
+            datilografa(fim2, delay=0.02)
+            exit()
 
